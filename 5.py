2e8662569eceeb7732205868865e2df9e74a42fd
HXY <305824707@qq.com>
2018-07-19 10:47:46 +0800

    5

---
+++ 5.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def find(self,s,low,high,temp,maxlen):
+
+        while low>= 0and high<=len(s)-1:
+            if s[high]==s[low]:
+                if (high-low+1)>maxlen:
+                    maxlen=high-low+1
+                    temp=s[low:high+1]
+                low-=1
+                high+=1
+            else:
+                break
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s)<=0:
+            return 0
+        temp=[]
+        maxlen=0
+        a=Solution()
+        for i,j in enumerate(s):
+            a.find(s,i,i,temp,maxlen)
+            a.find(s,i,i+1,temp,maxlen)
+        return temp,maxlen
+
+if __name__=="__main__":
+    s=Solution()
+    print(s.longestPalindrome("cbbd"))
+
